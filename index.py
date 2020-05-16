ca8a056d30b10899c1bfc324603220745922ad4d
H0ppyF33t <22322822+h0ppyf33t@users.noreply.github.com>
2020-05-16 17:30:00 +0530

    update index

--- index.py
+++ index.py
@@ -2,8 +2,7 @@
 # This project is a compilation of python scripts that hardens the security posture of an AWS S3 bucket.
 
 # As we know all AWS S3 buckets are globally accessible via this endpoint http://<bucket_name>.s3.amazonaws.com.
-# This is great and very easy to use but, not entirely secure as is. There are a few things you need enabled or
-# tweaked to ensure whatever you put in your bucket is safe and secure.
+# There are a few things you need enabled or tweaked to ensure whatever you put in your bucket is safe and secure.
 #
 # Here's what this project helps you automate:
 # 1. Enable SSE (Server Side Encryption) for your bucket so data at rest are encrypted.
@@ -24,6 +23,7 @@ SNS_TOPIC_ARN = os.getenv('SNS_TOPIC_ARN','')
 s3 = boto3.resource('s3')
 def lambda_handler(event, context):
     text_output_array = ["-------------------------\n"]
+    # I know this is not the perfect way to do this. LMK if you have another way to get the account number.
     ACCOUNT_ID = context.invoked_function_arn.split(":")[4]
     text_output_array.append("AWS ACCOUNT_ID: ")
     text_output_array.append(ACCOUNT_ID)
